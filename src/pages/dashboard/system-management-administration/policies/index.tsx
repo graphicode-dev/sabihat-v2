7f575009aca4cbbcc27d06ebff9f45b6f7d8f728
beloyosry <byosry@gmail.com>
2025-06-16 16:47:51 +0300

    finished policies

--- src/pages/dashboard/system-management-administration/policies/index.tsx
+++ src/pages/dashboard/system-management-administration/policies/index.tsx
@@ -1,5 +1,76 @@
+import { DynamicTable } from "../../../../components/table";
+import PageLayout from "../../../../layout/PageLayout";
+import { TableColumn, TableData } from "../../../../types/table";
+import { useNavigate } from "react-router-dom";
+
 function PoliciesPage() {
-    return <div>PoliciesPage</div>;
+    const navigate = useNavigate();
+    const columns: TableColumn[] = [
+        {
+            id: "1",
+            header: "Title",
+            accessorKey: "title",
+        },
+        {
+            id: "2",
+            header: "Description",
+            accessorKey: "description",
+        },
+    ];
+
+    const data: TableData[] = [
+        {
+            id: "1",
+            columns: {
+                title: "*****",
+                description: "*****",
+            },
+        },
+        {
+            id: "2",
+            columns: {
+                title: "*****",
+                description: "*****",
+            },
+        },
+        {
+            id: "3",
+            columns: {
+                title: "*****",
+                description: "*****",
+            },
+        },
+        {
+            id: "4",
+            columns: {
+                title: "*****",
+                description: "*****",
+            },
+        },
+        {
+            id: "5",
+            columns: {
+                title: "*****",
+                description: "*****",
+            },
+        },
+    ];
+
+    return (
+        <PageLayout>
+            <DynamicTable
+                title="Policies"
+                data={data}
+                columns={columns}
+                initialView="grid"
+                itemsPerPage={10}
+                addLabel="Add Policy"
+                onAddClick={() =>
+                    navigate("/system-management-administration/policies/add")
+                }
+            />
+        </PageLayout>
+    );
 }
 
 export default PoliciesPage;
--- src/pages/dashboard/system-management-administration/policies/view/index.tsx
+++ src/pages/dashboard/system-management-administration/policies/view/index.tsx
@@ -1,5 +1,49 @@
+import PageLayout from "../../../../../layout/PageLayout";
+import ViewCard from "../../../../../components/ui/ViewCard";
+// import { useParams } from "react-router-dom";
+import { TableData } from "../../../../../types/table";
+import { ViewCardData } from "../../../../../types";
+
 function PoliciesViewPage() {
-    return <div>PoliciesViewPage</div>;
+    // const { id } = useParams();
+
+    const data: TableData = {
+        id: "1",
+        columns: {
+            title: "*****",
+            description: "*****",
+        },
+    };
+
+    return (
+        <PageLayout>
+            <ViewCard
+                variant="default"
+                data={{
+                    rows: [
+                        {
+                            fields: [
+                                {
+                                    label: "Title",
+                                    value: data?.columns.title.toString(),
+                                },
+                            ],
+                        },
+                        {
+                            fields: [
+                                {
+                                    label: "Description",
+                                    value: data?.columns.description.toString(),
+                                    colSpan: 3,
+                                },
+                            ],
+                        },
+                    ],
+                }}
+                buttons
+            />
+        </PageLayout>
+    );
 }
 
 export default PoliciesViewPage;
