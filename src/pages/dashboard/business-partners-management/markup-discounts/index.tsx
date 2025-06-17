a904999e35f971217ac0f131d2345ac9b8fe2eef
beloyosry <byosry@gmail.com>
2025-06-17 09:38:42 +0300

    finished MarkUp

--- src/pages/dashboard/business-partners-management/markup-discounts/index.tsx
+++ src/pages/dashboard/business-partners-management/markup-discounts/index.tsx
@@ -1,5 +1,111 @@
+import { useNavigate } from "react-router-dom";
+import { DynamicTable } from "../../../../components/table";
+import PageLayout from "../../../../layout/PageLayout";
+import { TableColumn, TableData } from "../../../../types/table";
+
 function MarkUpPage() {
-    return <div>MarkUpPage</div>;
+    const navigate = useNavigate();
+    const columns: TableColumn[] = [
+        {
+            id: "partner",
+            header: "Partner",
+            accessorKey: "partner",
+        },
+        {
+            id: "class",
+            header: "Class",
+            accessorKey: "class",
+        },
+        {
+            id: "commissionType",
+            header: "Commission Type",
+            accessorKey: "commissionType",
+        },
+        {
+            id: "commissionValue",
+            header: "Commission Value",
+            accessorKey: "commissionValue",
+        },
+        {
+            id: "visitType",
+            header: "Visit Type",
+            accessorKey: "visitType",
+        },
+    ];
+    const data: TableData[] = [
+        {
+            id: "1",
+            columns: {
+                partner: "*****",
+                class: "*****",
+                commissionType: "*****",
+                commissionValue: "*****",
+                visitType: "*****",
+            },
+        },
+        {
+            id: "2",
+            columns: {
+                partner: "*****",
+                class: "*****",
+                commissionType: "*****",
+                commissionValue: "*****",
+                visitType: "*****",
+            },
+        },
+        {
+            id: "3",
+            columns: {
+                partner: "*****",
+                class: "*****",
+                commissionType: "*****",
+                commissionValue: "*****",
+                visitType: "*****",
+            },
+        },
+        {
+            id: "4",
+            columns: {
+                partner: "*****",
+                class: "*****",
+                commissionType: "*****",
+                commissionValue: "*****",
+                visitType: "*****",
+            },
+        },
+        {
+            id: "5",
+            columns: {
+                partner: "*****",
+                class: "*****",
+                commissionType: "*****",
+                commissionValue: "*****",
+                visitType: "*****",
+            },
+        },
+    ];
+
+    return (
+        <PageLayout>
+            <DynamicTable
+                title="All  Markup & Discounts Board"
+                data={data}
+                columns={columns}
+                addLabel="Add Markup & Discounts Board"
+                onAddClick={() => {
+                    navigate(
+                        `/business-partners-management/markup-discounts/add`
+                    );
+                }}
+                onRowClick={(rowId) =>
+                    navigate(
+                        `/business-partners-management/markup-discounts/view/${rowId}`
+                    )
+                }
+                hideBorder
+            />
+        </PageLayout>
+    );
 }
 
 export default MarkUpPage;
--- src/pages/dashboard/business-partners-management/markup-discounts/view/index.tsx
+++ src/pages/dashboard/business-partners-management/markup-discounts/view/index.tsx
@@ -1,5 +1,60 @@
+import ViewCard from "../../../../../components/ui/ViewCard";
+import PageLayout from "../../../../../layout/PageLayout";
+import { ViewCardData } from "../../../../../types";
+import { TableData } from "../../../../../types/table";
+
 function MarkUpViewPage() {
-    return <div>MarkUpViewPage</div>;
+    // const { id } = useParams();
+
+    const data: TableData = {
+        id: "1",
+        columns: {
+            partnerLayer: "*****",
+            partner: "*****",
+            class: "*****",
+            servicesType: "*****",
+            passengerType: "*****",
+            ticketType: "*****",
+            cabin: "*****",
+            portFrom: "*****",
+            portTo: "*****",
+            visitType: "*****",
+            MarkupDiscount: "*****",
+            MarkupDiscountType: "*****",
+            MarkupDiscountValue: "*****",
+            EffectiveDate: "*****",
+            EndDate: "*****",
+        },
+    };
+
+    return (
+        <PageLayout showBorder>
+            <ViewCard
+                variant="default"
+                data={
+                    {
+                        "Partner Layer": data.columns.partnerLayer,
+                        Partner: data.columns.partner,
+                        Class: data.columns.class,
+                        "Services Type": data.columns.servicesType,
+                        "Passenger Type": data.columns.passengerType,
+                        "Ticket Type": data.columns.ticketType,
+                        Cabin: data.columns.cabin,
+                        "Port From": data.columns.portFrom,
+                        "Port To": data.columns.portTo,
+                        "Visit Type": data.columns.visitType,
+                        "Markup/Discount": data.columns.MarkupDiscount,
+                        "Markup/Discount Type": data.columns.MarkupDiscountType,
+                        "Markup/Discount Value": data.columns.MarkupDiscountValue,
+                        "Effective Date": data.columns.EffectiveDate,
+                        "End Date": data.columns.EndDate,
+                    } as ViewCardData
+                }
+                buttons
+                hideBorder
+            />
+        </PageLayout>
+    );
 }
 
 export default MarkUpViewPage;
