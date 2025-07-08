dfd73b8c068b0015a54d19df55c73dc5f32ba11c
beloyosry <byosry@gmail.com>
2025-07-08 18:32:56 +0300

    updated initialSectionsToShow in ViewCard

--- src/components/ui/ViewCard.tsx
+++ src/components/ui/ViewCard.tsx
@@ -175,7 +175,7 @@ const ViewCard: React.FC<ViewCardProps> = ({
     const [showAllContent, setShowAllContent] = useState(false);
 
     // Default number of sections to show initially
-    const initialSectionsToShow = 6;
+    const initialSectionsToShow = 3;
 
     // Get rows for the Show More/Less feature
     const allRows = data?.rows || [];
